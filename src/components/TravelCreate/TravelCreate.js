0381a2d9b59c14bd4cb795c8604f774bcc909036
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-08 09:53:07 -0600

    added blank schema to recipe create

--- src/components/TravelCreate/TravelCreate.js
+++ src/components/TravelCreate/TravelCreate.js
@@ -3,6 +3,23 @@ import axios from 'axios'
 import { useState } from 'react'
 
 const TravelCreate = () => {
+    const [newTravel, setNewTravel] = useState({
+        name: '',
+        location: '',
+        population: '',
+        discover: [{
+            name: String,
+            description: String,
+            imageURL: String
+        }],
+        food: [{
+            place: String,
+            stars: Number,
+            description: String
+        }],
+        date: Date,
+        weather: String
+    })
 
     
     const travelCreate = async () => {
@@ -13,7 +30,12 @@ const TravelCreate = () => {
     <div className='travel-create'>
         <form className='travel-form'>
             <input placeholder='Enter City Name...'></input>
-
+            <input></input>
+            <input></input>
+            <input></input>
+            <input></input>
+            <input></input>
+            <input></input>
         </form>
 
     </div>
