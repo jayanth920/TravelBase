986b2ffaf7c1c1c0e4e2900a7046f51685b87d66
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-07 19:07:07 -0600

    axios request added to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // Components 
 import Header from './components/Header/Header.js' 
 
+
 function App() {
+  const [travels, setTravels] = useState([])
+
+  useEffect(() => {
+    getTravels()
+  }, [travels])
+
+  const getTravels = async () => {
+    await axios({
+      method: 'get',
+      url: ''
+    })
+    .then(res => 
+      setTravels(res.data))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div>
-      <Header />
+      <div className='app-body'>
+        <Header />
+      </div>
+
+      <Routes>
+        
+      </Routes>
     </div>
   )
 }
