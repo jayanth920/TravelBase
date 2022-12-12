c8001db0b5b5e1504e4567bcf117b8954aef6b93
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-12 13:11:14 -0600

    update works on all fields

--- src/components/TravelUpdate/TravelUpdate.js
+++ src/components/TravelUpdate/TravelUpdate.js
@@ -8,57 +8,37 @@ import TravelDelete from '../TravelDelete/TravelDelete'
 
 const TravelUpdate = ({id, travels}) => {
   const travelsData = travels
-  const [population, setPopulation] = useState('')
+  const [population, setPopulation] = useState(0)
+  const [location, setLocation] = useState('')
   const [name, setName] = useState('')
-  // console.log(travelsData[0].name);
-  let popData = population
+  const [discoverName, setDiscoverName] = useState('')
+  const [discoverDes, setDiscoverDes] = useState('')
+  const [discoverImage, setDiscoverImage] = useState('')
+  const [foodPlace, setFoodPlace] = useState('')
+  const [foodStars, setFoodStars] = useState('')
+  const [foodDes, setFoodDes] = useState('')
+  const [date, setDate] = useState('')
+  const [weather, setWeather] = useState('')
 
-    const [form, setForm] = useState({
-        name: undefined,
-        location: undefined,
-        population: popData,
-        discover:{
-          name: '',
-          description: '',
-          imageURL: ''
-        },
-        food:{
-          place: '',
-          stars: '',
-          description: ''
-        },
-        date: undefined,
-        weather: undefined,
-        _id: id
-      })
-
-
-      // const travelRefresh = async () => {
-      //   try {
-      //       const change = await axios.put('https://backend-travelapp.fly.dev/update', form)
-      //   } catch (err) {
-      //       console.log(err);
-      //   }
-      // }
       
       const travelRefresh = async () => {
         try {
             const change = await axios.put('https://backend-travelapp.fly.dev/update', {
               name: name,
-              location: undefined,
+              location: location,
               population: population,
               discover:{
-                name: '',
-                description: '',
-                imageURL: ''
+                name: discoverName,
+                description: discoverDes,
+                imageURL: discoverImage
               },
               food:{
-                place: '',
-                stars: '',
-                description: ''
+                place: foodPlace,
+                stars: foodStars,
+                description:foodDes
               },
-              date: undefined,
-              weather: undefined,
+              date: date,
+              weather: weather,
               _id: id
             })
         } catch (err) {
@@ -66,28 +46,20 @@ const TravelUpdate = ({id, travels}) => {
         }
       }
 
-      const handleChange  = (e) => {
-        const newObj = {...form}
-
-        newObj[e.target.name] = e.target.value
-        setForm(newObj)
-        console.log(form)
-      }
-
   return (
     <div>
         <form className='recipe-update-form'>
             <input placeholder='City Name' name='name' onChange={(e) => {setName(e.target.value)}} required></input>
-            <input placeholder='Location' name='location' value={form.location} onChange={handleChange} required></input>
+            <input placeholder='Location' name='location' onChange={(e) => {setLocation(e.target.value)}} required></input>
             <input placeholder='Population' name='population' onChange={(e) => {setPopulation(e.target.value)}} required></input>
-            <input placeholder='Place to Discover' name='discover.name' value={form.discover.name} onChange={handleChange} required></input>
-            <input placeholder='Place Description' name='discover.description' value={form.discover.description} onChange={handleChange} required></input>
-            {/* <input placeholder='Place Image URL' name='discover.imageURL' value={form.discover.imageURL} onChange={handleChange} required></input> */}
-            <input placeholder='Place to Eat' name='food.place' value={form.food.place} onChange={handleChange} required></input>
-            <input placeholder='Stars Rating' name='food.stars' value={form.food.stars} onChange={handleChange} required></input>
-            <input placeholder='Food Place Description' name='food.description' value={form.food.description} onChange={handleChange} required></input>
-            <input placeholder='Date Entered MM-DD-YYYY' name='date' value={form.date} onChange={handleChange} required></input>
-            <input placeholder='Weather' name='weather' value={form.weather} onChange={handleChange} required></input>
+            <input placeholder='Place to Discover' onChange={(e) => {setDiscoverName(e.target.value)}} required></input>
+            <input placeholder='Place Description' onChange={(e) => {setDiscoverDes(e.target.value)}} required></input>
+            <input placeholder='Place Image URL' onChange={(e) => {setDiscoverImage(e.target.value)}} required></input>
+            <input placeholder='Place to Eat' onChange={(e) => {setFoodPlace(e.target.value)}} required></input>
+            <input placeholder='Stars Rating' onChange={(e) => {setFoodStars(e.target.value)}} required></input>
+            <input placeholder='Food Place Description' onChange={(e) => {setFoodDes(e.target.value)}} required></input>
+            <input placeholder='Date Entered MM-DD-YYYY' onChange={(e) => {setDate(e.target.value)}} required></input>
+            <input placeholder='Weather' onChange={(e) => {setWeather(e.target.value)}} required></input>
         </form>
         <button className='update-button' onClick={travelRefresh}>Update Recipe</button>
         <Link to='/'><TravelDelete id={id} /></Link>
