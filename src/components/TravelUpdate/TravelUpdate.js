dc6ab54c1fca582267fd82c1bfb8ec66b47d795e
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-12 16:14:22 -0600

    style

--- src/components/TravelUpdate/TravelUpdate.js
+++ src/components/TravelUpdate/TravelUpdate.js
@@ -77,7 +77,7 @@ const TravelUpdate = ({id, travels, travelSearch}) => {
 
   return (
     <div>
-      <h1 className='update-title'></h1>
+      <h1 className='update-title'>Update Here 🚀</h1>
         <form className='recipe-update-form'>
             <input className='input-elements'placeholder='City Name' name='name' onChange={(e) => {setName(e.target.value)}} required></input>
             <input className='input-elements'placeholder='Location' name='location' onChange={(e) => {setLocation(e.target.value)}} required></input>
