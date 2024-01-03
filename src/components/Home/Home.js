a737a555cd0a468c8130f1e0f9cbe0422da97d57
jayanth920 <jayanthgajula92004@gmail.com>
2024-01-03 01:33:15 -0600

    LINK UPDATE *** BACKEND

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,32 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import './Home.css'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
+const Home = ({travels}) => {
+  const [travelList, setTravelList] = useState('');
 
-const Home = ({travels, getTravels}) => {
-  const [travelList, setTravelList] = useState('')
-
-  const getList = () => {
+  const getList = useCallback(() => {
     let travelMap = travels.map((travels, index) => {
-      return <div className='travel-list' key={index}>
-        <Link to={'/travels/'+ travels._id}>
-          <div className='name'>{travels.name}</div>
-        </Link>
-            </div>
-    })
-    setTravelList(travelMap)
-  }
+      return (
+        <div className='travel-list' key={index}>
+          <Link to={'/travels/' + travels._id}>
+            <div className='name'>{travels.name}</div>
+          </Link>
+        </div>
+      );
+    });
+    setTravelList(travelMap);
+  }, [travels]);
 
   useEffect(() => {
-    getList()
-  },[travels])
-
-  const refresh = () => {
-    setTravelList([])
-    getTravels()
-  }
-
-
+    getList();
+  }, [travels, getList]);
 
   return (
     <div className='home'>
-      <div className='travel-list-container'>
-        {travelList}
-      </div>
-      {/* <button onClick={refresh}>Refresh Demo</button> */}
+      <div className='travel-list-container'>{travelList}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
